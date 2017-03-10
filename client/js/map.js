b2895e01b89f5a464dc9a192ee7a138dabe6cb18
Ted Ngok <tedngok@gmail.com>
2017-03-11 07:46:16 +0800

    react google map

--- client/js/map.js
+++ client/js/map.js
@@ -1,26 +1,70 @@
-// class Map extends React.Component {
-//   constructor(props){
-//     super(props)
-//   }
-//
-//   render(){
-//     const map = new google.maps.Map(document.getElementById('content') , {
-//           center: {lat: -34.397, lng: 150.644},
-//           scrollwheel: false,
-//           zoom: 8
-//         });
-//     return (
-//       map
-//     );
-//   }
-// };
-//
-// ReactDOM.render(
-//   <Map />
-//   ,document.getElementById('content')
-// );
-var map = new google.maps.Map(document.getElementById('cont'), {
-          center: {lat: -34.397, lng: 150.644},
-          scrollwheel: false,
-          zoom: 8
-        });
+class Map extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      map: null,
+      markers: null,
+    }
+  }
+
+  componentWillMount(){
+    this.fetchMarker();
+  }
+
+  componentDidMount(){
+    this.initMap();
+  }
+
+  shouldComponentUpdate(nexProps, nextState){
+    if (nextState.markers){
+      this.fetchMarker();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async fetchMarker() {
+    try {
+      const result = await fetch ("http://localhost/coursework/api/api.php?format=json&lang=en");
+      const json = await result.json();
+
+      this.setState({markers: json.stationList.station });
+
+    } catch (e){
+      console.log(e);
+    }
+  }
+
+  initMarker(){
+    for (let markerJSON of this.state.markers){
+      const marker = new google.maps.Marker({
+        position: {lat: parseFloat(markerJSON.lat), lng: parseFloat(markerJSON.lng)}
+      });
+      marker.setMap(this.state.map);
+    }
+  }
+
+  initMap(){
+    const map = new google.maps.Map(this.refs.map, {
+      center: {lat: 22.25, lng: 114.1667},
+      zoom: 10
+    });
+
+    this.setState({map: map});
+  }
+
+  render(){
+    return (
+      <div>
+        <div>123</div>
+        <div id="map" ref="map"></div>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(
+  <Map />
+  ,document.getElementById('content')
+);
