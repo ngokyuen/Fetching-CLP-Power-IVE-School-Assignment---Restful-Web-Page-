57a17c0a7e8030e317c2135abe9a9b741fd49255
ngokyuen <tedngok@gmail.com>
2017-03-30 07:55:19 +0800

    fix css

--- client/js/menu.js
+++ client/js/menu.js
@@ -15,23 +15,17 @@ class Menu extends React.Component {
       <div className="menuContainer">
         <ul>
           <li ><a href="">Main</a></li>
-          <li ><div className="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="false" data-auto-logout-link="true"></div></li>
           <li ><a href="javascript:;">Contact us</a></li>
           <li ><a href="./admin.html">Admin</a></li>
         </ul>
-        <div className="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+        <div className="facebook-like">
+          <div className="fb-like " data-href="https://developers.facebook.com/docs/plugins/" data-layout="box_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+        </div>
+
       </div>
     )
   }
 }
-
-// var Test = React.createClass({
-//   render: function() {
-//     return (
-//       <div>234</div>
-//     );
-//   }
-// });
 
 ReactDOM.render(
   <Menu />
