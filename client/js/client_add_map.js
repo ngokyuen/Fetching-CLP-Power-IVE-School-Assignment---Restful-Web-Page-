f313e17caf13479e67ce81cc2a2bcbe481cf8d75
ted.ngok <tedngok@gmail.com>
2017-04-05 18:00:46 +0800

    improve client add map function phrase 1

--- client/js/client_add_map.js
+++ client/js/client_add_map.js
@@ -7,9 +7,35 @@ class ClientAddMapComponent extends React.Component {
     super(props);
     this.state = {
       provider: '',
+      clientAddMarkersDetail: [],
+      showMarkerDetailDialog: false,
+      tempMarkerDetail: {
+        index: 0,
+        lat: 0,
+        lng: 0,
+        districtL: '',
+        districtS: '',
+        location: '',
+        address: '',
+        type: '',
+        img: '',
+        parkingNo: '',
+        lang: 'EN',
+      }
     }
   }
 
+//update detail for client added map
+  updateClientAddMapDetail(){
+
+  }
+
+//send request for delete client added map
+  deleteClientAddMap(){
+
+  }
+
+//send request for upload client added map to server
   submitClientAddMap(){
 
   }
@@ -18,14 +44,78 @@ class ClientAddMapComponent extends React.Component {
     this.setState({provider: e.target.value});
   }
 
+  openMarkerDetailDialog(){
+    this.setState({showMarkerDetailDialog: true});
+    //this.renderMarkerDetailDialog(index);
+  }
+
+  closeMarkerDetailDialog(){
+    this.setState({showMarkerDetailDialog: false});
+  }
+
+  changeLocation(e){
+  }
+
+  renderMarkerDetailDialog(){
+    if (this.state.showMarkerDetailDialog){
+      return (
+        <div className="alertContainer">
+          <div className="background" onClick={this.closeMarkerDetailDialog.bind(this)}></div>
+          <div className="alert">
+            <div className="content">
+              <div className="content_content table">
+                <div className="row">
+                  <div className="column">Location:</div>
+                  <div className="column"><input onChange={this.changeLocation.bind(this)} value={this.state.tempMarkerDetail.location} /></div>
+                </div>
+                <div className="row">
+                  <div className="column">Address:</div>
+                  <div className="column"><input value={this.state.tempMarkerDetail.address} /></div>
+                </div>
+                <div className="row">
+                  <div className="column">District:</div>
+                  <div className="column"><input value={this.state.tempMarkerDetail.districtL} /></div>
+                </div>
+                <div className="row">
+                  <div className="column">District2:</div>
+                  <div className="column"><input value={this.state.tempMarkerDetail.districtS} /></div>
+                </div>
+                <div className="row">
+                  <div className="column">Type:</div>
+                  <div className="column"><input value={this.state.tempMarkerDetail.type} /></div>
+                </div>
+                <div className="row">
+                  <div className="column">Image:</div>
+                  <div className="column"><input value={this.state.tempMarkerDetail.img} /></div>
+                </div>
+                <div className="row">
+                  <div className="column">Parking No:</div>
+                  <div className="column"><input value={this.state.tempMarkerDetail.parkingNo} /></div>
+                </div>
+              </div>
+              <div className="bottom_buttons">
+                <button onClick={this.updateClientAddMapDetail.bind(this)}>Submit</button>
+                <button onClick={this.deleteClientAddMap.bind(this)}>Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return null;
+    }
+
+  }
+
   renderClientAddMarkers(){
     const {clientAddMarkers} = this.props.Map;
     return (
       <div className="clientAddMarkers">
+        {this.renderMarkerDetailDialog()}
         <div className="title">Your Recommendation</div>
         {clientAddMarkers.map((clientAddMarker, index)=>{
           return (
-            <div key={index} className="clientAddMarker">
+            <div key={index} className="clientAddMarker" onClick={this.openMarkerDetailDialog.bind(this)}>
                 <div><img src="./img/flag3_565_720.png" /></div>
                 <div>{index+1}</div>
             </div>
